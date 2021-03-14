b1cba20f40107ba432255ff097f2490126804b10
el3031 <67562724+el3031@users.noreply.github.com>
2021-03-14 14:50:46 -0700

    updated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,29 @@
 from facebook_scraper import get_posts
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
+import json
 
 page_name = 'columbiaconfessionz'
 
 posts = []
-for post in get_posts(page_name, pages=10):
-  posts.append(post['text'])
-print(len(posts))
 
-compoundScores = []
+output_data = []
+
 analyzer = SentimentIntensityAnalyzer()
-for post in posts:
-    vs = analyzer.polarity_scores(post)
-    compoundScores.append(vs['compound'])
-    print("{:-<65} {}".format(post, str(vs['compound'])))
+
+for i, post in enumerate(get_posts(page_name, pages=10)):
+    if post['text'] is not None:
+        vs = analyzer.polarity_scores(post['text'])
+        post['time'] = post['time'].strftime('%m/%d/%Y')
+
+        post['sentiment'] = vs['compound']
+
+        post_num = post['text'].split(' ')[0].replace('.', '')
+
+        post["confession_num"] = post_num
+
+        #x = mycol.insert_one(post)
+
+        output_data.append(post)
+
+print(output_data)
