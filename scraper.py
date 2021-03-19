69386ee98d24511b193eda6629830d58b04230ff
el3031 <67562724+el3031@users.noreply.github.com>
2021-03-18 18:07:26 -0700

    updated mycol.count() to use count_documents() instead because count() is no longer a supported function

--- scraper.py
+++ scraper.py
@@ -2,6 +2,24 @@ from facebook_scraper import get_posts
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import json
+from facebook_scraper import get_posts
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+from pprint import pprint
+import os
+
+# Load environment variables
+load_dotenv(find_dotenv())
+
+# Connect to MongoDB
+MDB_USERNAME = os.getenv('MDB_USERNAME')
+MDB_PASSWORD = os.getenv('MDB_PASSWORD')
+MDB_URI = f'mongodb+srv://{MDB_USERNAME}:{MDB_PASSWORD}@confessions.scrus.mongodb.net/confessions?retryWrites=true&w=majority'
+client = MongoClient(MDB_URI)
+
+mydb = client["confessions_database"]
+mycol = mydb["confessions"]
 
 page_name = 'columbiaconfessionz'
 
@@ -19,12 +37,26 @@ for i, post in enumerate(get_posts(page_name, pages=3)):
         time = post['time'].strftime('%m/%d/%Y')
         post_id = post["post_id"]
         post_url = post["post_url"]
-        confession_num = int(post['text'].split(' ')[0].replace('.', ''))
         
-        list_of_idx = [0]
+        #find first confession_num
+        first_conf_num = ''
+        found_num = False
+        k = 0
+        while not found_num:
+            if post['text'][k].isdigit():
+                first_conf_num = first_conf_num + post['text'][k]
+                if not post['text'][k+1].isdigit():
+                    found_num = True
+            k+=1
+                
+        
+        
+        confession_num = int(first_conf_num)
+        
+        #finds index of next post num and appends to list
+        list_of_idx = []
         next_post = True
-        
-        confession_nums = [confession_num]
+        confession_nums = []
         
         next_num = confession_num
         while next_post:
@@ -42,7 +74,7 @@ for i, post in enumerate(get_posts(page_name, pages=3)):
             
         
         posts_in_post = []
-        for idx in range(1,len(list_of_idx)):
+        for idx in range(0,len(list_of_idx)):
             if list_of_idx[idx] != -1:
                 posts_in_post.append(post['text'][list_of_idx[idx]:list_of_idx[idx+1]])
         #print(posts_in_post)    
@@ -57,7 +89,7 @@ for i, post in enumerate(get_posts(page_name, pages=3)):
                 'shares': post['shares'],
                 'time': time,
                 'post_url': post_url,
-                'confession_num': confession_nums[j+1]
+                'confession_num': confession_nums[j]
             }
 
             if post_id:  
@@ -72,10 +104,9 @@ print(output_data)
 
 for post_data in output_data:
     myquery = {"id_": post_data["_id"]}
-    results = mycol.find(myquery);
-    
-    
-    if (results.count() == 0):
+    results = mycol.count_documents(myquery)
+
+    if (results == 0):
         mycol.insert_one(post_data)
         print("not found, inserted")
     else:
